436cf565fec9f0061e60801e39d5cb35436c1b81
Yurino Oga <yurino.o@latona.io>
2022-01-17 16:20:02 +0900

    release

--- star_sql_general_data.sql
+++ star_sql_general_data.sql
@@ -1,13 +1,14 @@
 CREATE TABLE `star_general_data`
 (
   `CodeName`                   varchar(20) NOT NULL,
+  `StarType`                   varchar(20) DEFAULT NULL,
   `Description`                varchar(40) DEFAULT NULL,
-  `CalenderType`               varchar(6) NOT NULL,
+  `CalenderType`               varchar(6) DEFAULT NULL,
   `BirthYear`                  varchar(20) DEFAULT NULL,
   `EstimatedDeathYear`         varchar(80) DEFAULT NULL,
   `Status`                     varchar(20) DEFAULT NULL,
   `CreatedDateTime`            varchar(20) DEFAULT NULL,
   `LastModifiedDateTime`       varchar(20) DEFAULT NULL,
-    PRIMARY KEY (`CodeName`),
+    PRIMARY KEY (`CodeName`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;

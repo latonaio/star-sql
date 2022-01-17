0ee32a40bcd7c0a740490a32eba4bfe0c70543b7
Yurino Oga <yurino.o@latona.io>
2022-01-17 17:46:35 +0900

    release

--- README.md
+++ README.md
@@ -1,20 +1,15 @@
 # star-sql    
 
-star-sql は、主にエッジアプリケーションにおいて、恒星データを保存するSQLテーブルを作成するためのレポジトリです。  
-star-sql は、そのままクラウド環境におけるアプリケーションにも、適用可能です。  
+star-sql は、主に宇宙で活用されるエッジアプリケーションにおいて、惑星データを保存するSQLテーブルを作成するためのレポジトリです。  
+star-sql は、そのまま宇宙ステーション等で利用されるクラウド環境におけるアプリケーションにも、適用可能です。  
 
 ## sqlの設定ファイル
 
 star-sql には、sqlの設定ファイルとして、以下のファイルが含まれています。  
 
-* star-sql-general-data.sql（恒星 - 一般データ）  
-* star-sql-observation-data.sql（恒星 - 観測データ）    
-* star-sql-orbital-characteristic-data.sql （恒星 - 軌道特性データ）   
+* star-sql-general-data.sql（恒星 - 一般データ）
+* star-sql-orbital-characteristic-data.sql（恒星 - 軌道特性データ）
 * star-sql-physical-characteristic-data.sql （恒星 - 物理特性データ）
-* star-sql-rotation-characteristic-data.sql（恒星 - 回転特性データ）  
-* star-sql-photospheric-composition-data.sql（恒星 - 光球組成データ） 
 
 ## MySQLのセットアップ / Kubernetesの設定 / SQLテーブルの作成方法
 MySQLのセットアップ / Kubernetesの設定 / 具体的なSQLテーブルの作成方法、については、[mysql-kube](https://github.com/latonaio/mysql-kube)を参照ください。
-
-
---
+++ star-sql-orbital-characteristic-data.sql
@@ -0,0 +1,30 @@
+CREATE TABLE `star_orbital_characteristic_data`
+(
+  `CodeName`                   varchar(20) NOT NULL,
+  `Aphelion`                   varchar(20) DEFAULT NULL,
+  `AphelionUnit`               varchar(5) DEFAULT NULL,
+  `Perihelion`                 varchar(20) DEFAULT NULL,
+  `PerihelionUnit`             varchar(5) DEFAULT NULL,
+  `SemiMajorAxis`              varchar(20) DEFAULT NULL,
+  `SemiMajorAxisUnit`          varchar(5) DEFAULT NULL,
+  `Eccentricity`               varchar(20) DEFAULT NULL,
+  `SiderealOrbitalPeriod`      varchar(20) DEFAULT NULL,
+  `SiderealOrbitalPeriodUnit`  varchar(5) DEFAULT NULL,
+  `SiderealOrbitalPeriod`      varchar(20) DEFAULT NULL,
+  `SiderealOrbitalPeriodUnit`  varchar(5) DEFAULT NULL,
+  `AverageOrbitalSpeed`        varchar(20) DEFAULT NULL,
+  `AverageOrbitalSpeedUnit`    varchar(5) DEFAULT NULL,
+  `MeanAnomaly`                varchar(20) DEFAULT NULL,
+  `MeanAnomalyUnit`            varchar(5) DEFAULT NULL,
+  `Inclination`                varchar(20) DEFAULT NULL,
+  `InclinationUnit`            varchar(5) DEFAULT NULL,
+  `LongitudeOfNode`            varchar(20) DEFAULT NULL,
+  `LongitudeOfNodeUnit`        varchar(5) DEFAULT NULL,
+  `ArgumentOfPerihelion`       varchar(20) DEFAULT NULL,
+  `ArgumentOfPerihelionUnit`   varchar(5) DEFAULT NULL,
+  `CreatedDateTime`            varchar(20) DEFAULT NULL,
+  `LastModifiedDateTime`       varchar(20) DEFAULT NULL,
+    PRIMARY KEY (`CodeName`),
+    CONSTRAINT `StarOrbitalCharacteristicData_fk` FOREIGN KEY (`CodeName`) REFERENCES `star_general_data` (`CodeName`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
